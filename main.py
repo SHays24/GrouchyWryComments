6a46260d66963801e2eefd38cd2007bee7ab5914
SaxonHays <2376514-SaxonHays@users.noreply.replit.com>
2023-05-25 12:42:29 +0000

    i commit

--- main.py
+++ main.py
@@ -1,8 +1,51 @@
 from tkinter import *
+from tkinter import filedialog
 from tkinter import ttk
+import sv_ttk
+import re
+
+
+class MainApplication(Tk):
+  def __init__(self):
+    self.started = False
+    super().__init__()
+    try:
+      open("settings.txt", "r")
+    except:
+      setFolder() 
+    self.title("Galeri")
+    chatterbox = PhotoImage(file = "./chatterbox.png")
+    self.iconphoto(False, chatterbox)
+    self.nb = ttk.Notebook(self)
+    sv_ttk.set_theme("light")
+  def setFolder():
+    folder = filedialog.askdirectory()
+    f = open("settings.txt", "wt")
+    f.write("folder:" + folder + ";")
+    f.close()
+  try:
+    open("settings.txt", "r")
+  except:
+    setFolder()
+  class PhotoRow(Tk):
+    def __init__(self):
+      f = open("settings.txt", "r")
+      file = re.search(r"(?<=folder:)\/[^;]+", f.read())
+      print(file.group())
+      return
+
+if __name__ == '__main__':
+  app = MainApplication()
+  row = MainApplication.PhotoRow()
+  app.mainloop()
+"""
+from tkinter import *
+from tkinter import ttk
+
 root = Tk()
 frm = ttk.Frame(root, padding=10)
 frm.grid()
 ttk.Label(frm, text="", wraplength=200).grid(column=0, row=0)
 ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
 root.mainloop()
+"""
