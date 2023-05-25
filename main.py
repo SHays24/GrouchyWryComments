f86950df1885c2e6396f599ea9ea8ccbf2409a8a
SHays24 <81133539+SHays24@users.noreply.github.com>
2023-05-25 16:53:11 +0000

    so i guess i commit now ;>

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from tkinter import filedialog
 from tkinter import ttk
 import sv_ttk
 import re
+import os
 
 
 class MainApplication(Tk):
@@ -18,6 +19,17 @@ class MainApplication(Tk):
     self.iconphoto(False, chatterbox)
     self.nb = ttk.Notebook(self)
     sv_ttk.set_theme("dark")
+    
+  def IndexFoto(directory, pos):
+    index = os.listdir(directory)
+    index.sort()
+    if pos < 0 | pos >= len(index):
+      return None
+    name = index[pos]
+    path = os.path.join(directory, name)
+    return path
+    
+    
   def setFolder():
     folder = filedialog.askdirectory()
     f = open("settings.txt", "wt")
